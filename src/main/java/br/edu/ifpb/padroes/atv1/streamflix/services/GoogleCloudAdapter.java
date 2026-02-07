c915b5331a5a2bf8aab4444f537dc3f497c36828
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2026-02-07 17:55:11 -0300

    criando o cloudAdapter

--- src/main/java/br/edu/ifpb/padroes/atv1/streamflix/services/GoogleCloudAdapter.java
+++ src/main/java/br/edu/ifpb/padroes/atv1/streamflix/services/GoogleCloudAdapter.java
@@ -1,5 +1,20 @@
 package br.edu.ifpb.padroes.atv1.streamflix.services;
 
-public class GoogleCloudAdapter {
+public class GoogleCloudAdapter implements StorageService{
+    private final GoogleCloudStorage gcsService;
+
+    public GoogleCloudAdapter(GoogleCloudStorage gcsService) {
+        this.gcsService = gcsService;
+    }
+
+    @Override
+    public byte[] download(String projectId, String fileId) {
+        return gcsService.retrieveFile(projectId, fileId);
+    }
+
+    @Override
+    public void upload(String projectId, String fileId, byte[] data) {
+        gcsService.storeFile(projectId, fileId, data);
+    }
 
 }
