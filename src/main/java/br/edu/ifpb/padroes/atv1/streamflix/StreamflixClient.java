ba9209116ddc7024f36db9776d1b48ab0cdceee9
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2026-02-07 17:59:12 -0300

    refatorando StreamFkixClient

--- src/main/java/br/edu/ifpb/padroes/atv1/streamflix/StreamflixClient.java
+++ src/main/java/br/edu/ifpb/padroes/atv1/streamflix/StreamflixClient.java
@@ -9,6 +9,12 @@ import br.edu.ifpb.padroes.atv1.streamflix.subtitle.SubtitleService;
 
 public class StreamflixClient {
 
+    private final StorageService storageService;
+
+    public StreamflixClient(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
     public void watchVideo(String userId, String token, String videoId) {
         // Cliente precisa conhecer TODOS os subsistemas!
         AuthenticationService auth = new AuthenticationService();
@@ -18,28 +24,9 @@ public class StreamflixClient {
 
         // Processo complexo e acoplado
         if (auth.authenticate(userId, token)) {
-            byte[] rawVideo = StorageService.dowload("videos", videoId);
+            byte[] rawVideo = storageService.download("videos", videoId);
             byte[] convertedVideo = converter.convert(rawVideo, "MP4");
-            String subs = subtitles.getSubtitles(videoId, "pt-BR");
-
-            Video video = new Video(videoId, "Movie Title", convertedVideo);
-            video.play();
-            streaming.startStream(convertedVideo);
-        }
-    }
-
-    // Se quiser usar Google Cloud, precisa duplicar código!
-    public void watchVideoFromGCS(String userId, String token, String videoId) {
-        AuthenticationService auth = new AuthenticationService();
-        GoogleCloudStorage gcs = new GoogleCloudStorage();
-        VideoConverter converter = new VideoConverter();
-        SubtitleService subtitles = new SubtitleService();
-        StreamingService streaming = new StreamingService();
-
-        if (auth.authenticate(userId, token)) {
-            byte[] rawVideo = gcs.retrieveFile("my-project", videoId);
-            byte[] convertedVideo = converter.convert(rawVideo, "MP4");
-            String subs = subtitles.getSubtitles(videoId, "pt-BR");
+            subtitles.getSubtitles(videoId, "pt-BR");
 
             Video video = new Video(videoId, "Movie Title", convertedVideo);
             video.play();
